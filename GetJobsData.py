0431a5298bd351990b16960637057501768fc5e9
Sam Silvia <s5silvia@student.bridgew.edu>
2020-06-22 17:21:09 -0400

    Changed layout - added filter to GUI

--- GetJobsData.py
+++ GetJobsData.py
@@ -1,82 +1,30 @@
 import requests
 import sqlite3
-import random
 import sys
-from PyQt5.QtWidgets import QApplication, QVBoxLayout
+# from PyQt5.QtWidgets import QApplication, QFormLayout
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtWidgets import QPushButton
-from PyQt5.QtWidgets import QListWidget
+# from PyQt5.QtWidgets import QListWidget
 from PyQt5.QtWidgets import QInputDialog
-from PyQt5.QtWidgets import QDialog
+# from PyQt5.QtWidgets import QDialog
 from PyQt5.QtWidgets import QFormLayout
-from PyQt5 import QtCore, QtWidgets
+# from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+# from PyQt5 import QtWidgets
 from PyQt5 import QtWidgets
 
 
-class TechnologyWindow(QWidget):
-
+class JobWindow(QWidget):
     def __init__(self, to_display):
         super().__init__()
-
-        layout = QFormLayout()
         self.data_to_display = to_display
         self.data_item_displayed = 0
-
-        self.hi = QPushButton("Hi")
-        layout.addWidget(self.hi)
-
-        self.setLayout(layout)
-        self.setWindowTitle("Technology Window")
-
-
-class LocationWindow(QWidget):
-
-    def __init__(self, to_display):
-        super().__init__()
-
+        current_data = self.data_to_display[self.data_item_displayed]
         layout = QFormLayout()
-        self.data_to_display = to_display
-        self.data_item_displayed = 0
-
-        self.hi = QPushButton("Hi")
-        layout.addWidget(self.hi)
-
-        self.setLayout(layout)
-        self.setWindowTitle("Location Window")
-
-
-class CompanyWindow(QWidget):
-
-    def __init__(self, to_display):
-        super().__init__()
-
-        layout = QFormLayout()
-        self.data_to_display = to_display
-        self.data_item_displayed = 0
-
-        self.hi = QPushButton("Hi")
-        layout.addWidget(self.hi)
-
-        self.setLayout(layout)
-        self.setWindowTitle("Location Window")
-
-
-class FilterWindow(QWidget):
-
-    def __init__(self, to_display):
-        super().__init__()
-
-        # ADDITIONAL WINDOWS
-        self.technology_window = TechnologyWindow(self)
-        self.location_window = LocationWindow(self)
-        self.company_window = CompanyWindow(self)
-
-        # Layout stuff
-        layout = QFormLayout()
-        self.data_to_display = to_display
-        self.data_item_displayed = 0
+        filters_caption = QLabel("Filters:")
+        layout.addRow(filters_caption)
 
         # TECHNOLOGY FILTER
         self.technology_filter_button = QPushButton("Technology Filter")
@@ -86,7 +34,7 @@ class FilterWindow(QWidget):
 
         # FIND TECHNOLOGY JOBS - switch to technology window
         self.technology_apply_button = QtWidgets.QPushButton('Apply Technology Filter')
-        self.technology_apply_button.clicked.connect(self.switch_technology_window)
+        # self.technology_apply_button.clicked.connect(self.switch_technology_window)
         layout.addWidget(self.technology_apply_button)
 
         # LOCATION FILTER
@@ -97,7 +45,7 @@ class FilterWindow(QWidget):
 
         # FIND LOCATION JOBS - switch to location window
         self.location_apply_button = QtWidgets.QPushButton('Apply Location Filter')
-        self.location_apply_button.clicked.connect(self.switch_location_window)
+        # self.location_apply_button.clicked.connect(self.switch_location_window)
         layout.addWidget(self.location_apply_button)
 
         # COMPANY FILTER
@@ -108,69 +56,69 @@ class FilterWindow(QWidget):
 
         # FIND COMPANY JOBS - switch to company window
         self.company_apply_button = QtWidgets.QPushButton('Apply Company Filter')
-        self.company_apply_button.clicked.connect(self.switch_company_window)
+        # self.company_apply_button.clicked.connect(self.switch_company_window)
         layout.addWidget(self.company_apply_button)
 
+        # Jobs are displayed in this line edit
+        jobs_caption = QLabel("Jobs:")
+        layout.addRow(jobs_caption)
+        title_caption = QLabel("Title:")
+        self.job_title = QLineEdit()
+        self.job_title.setReadOnly(True)
+        self.job_title.setFixedWidth(500)
+        self.job_title.setText(current_data['title'])
+        layout.addRow(title_caption, self.job_title)
+
+        # Get more information
+        get_more_info = QPushButton("Get More Information")
+        self.job_description = QLineEdit()
+        self.job_description.setReadOnly(True)
+        self.job_description.setFixedWidth(500)
+        layout.addRow(get_more_info, self.job_description)
+
+        # Get next job
+        get_next_job = QPushButton("Get Next Job")
+        layout.addRow(get_next_job)
+
+        # Button presses
+        get_more_info.pressed.connect(self.show_more_information)
+        get_next_job.pressed.connect(self.show_next_job)
+
         self.setLayout(layout)
-        self.setWindowTitle("Filter Window")
+        self.setWindowTitle("Job Search")
+
+    def show_next_job(self):
+        self.data_item_displayed += 1
+        current_data = self.data_to_display[self.data_item_displayed]
+        self.job_title.setText(current_data['title'])
+
+    def show_more_information(self):
+        self.data_item_displayed += 1
+        current_data = self.data_to_display[self.data_item_displayed]
+        self.job_description.setText(current_data['description'])
 
     # GET USER INPUT FOR FILTERS
     def get_technology(self):
         technology_input, ok = QInputDialog.getText(self, 'Location Filter', 'Enter location:')
         if ok:
             self.technology_line_edit.setText(str(technology_input))
+            return technology_input
 
     def get_location(self):
         location_input, ok = QInputDialog.getText(self, 'Location Filter', 'Enter location:')
         if ok:
             self.location_line_edit.setText(str(location_input))
+            return location_input
 
     def get_company(self):
         company_input, ok = QInputDialog.getText(self, "Company Filter", "Enter company")
         if ok:
             self.company_line_edit.setText(str(company_input))
-
-    # SWITCH TO NEW WINDOWS FOR EACH FILTER
-    def switch_technology_window(self):
-        self.technology_window.show()
-
-    def switch_location_window(self):
-        self.location_window.show()
-
-    def switch_company_window(self):
-        self.company_window.show()
+            return company_input
 
 
-class TechnologyLayout(QWidget):
-
-    def __init__(self, to_display):
-        super().__init__()
-
-        layout = QFormLayout()
-        self.data_to_display = to_display
-        self.data_item_displayed = 0
-
-        # TECHNOLOGY FILTER
-        self.technology_filter_button = QPushButton("Technology Filter")
-        self.technology_filter_button.clicked.connect(self.get_technology)
-        self.technology_line_edit = QLineEdit()
-        layout.addRow(self.technology_filter_button, self.technology_line_edit)
-
-        # FIND TECHNOLOGY JOBS - switch to technology window
-        self.technology_apply_button = QtWidgets.QPushButton('Apply Technology Filter')
-        self.technology_apply_button.clicked.connect(self.technology_window)
-        layout.addWidget(self.technology_apply_button)
-
-        self.setLayout(layout)
-        self.setWindowTitle("Technology Window")
-
-
-# print company name to terminal
 def display_data(to_display):
-    # for job in to_display:
-    #     print(job['company'].get('display_name'))
-
-    window = FilterWindow(to_display)
+    window = JobWindow(to_display)
     return window
 
 
@@ -181,29 +129,18 @@ def write_data(data, filename='jobs_data.txt'):
             print(job, file=file)
 
 
-def get_data():
-    # key_word = ['python', 'java', 'golang', 'javascript', 'devops', 'database', 'web', 'design'] location =
-    # f"http://api.adzuna.com/v1/api/jobs/us/search/1?app_id=18381bc0&app_key=f20a9d4e1c0d42e8d120af190ecfb44d" \
-    # f"&results_per_page=20&what={random.choice(key_word)}"
-
-    # key_word = input("What key word do you want to search for?:") location =
-    # f"http://api.adzuna.com/v1/api/jobs/us/search/1?app_id=18381bc0&app_key=f20a9d4e1c0d42e8d120af190ecfb44d" \
-    # f"&results_per_page=20&what={key_word}"
-
-    location = f"http://api.adzuna.com/v1/api/jobs/us/search/1?app_id=18381bc0&app_key=f20a9d4e1c0d42e8d120af190ecfb44d" \
-               f"&results_per_page=20"
+def get_data(location):
     response = requests.get(location)
     if response.status_code != 200:
         return []
     data = response.json()
-    print(location)
     return data["results"]
 
 
 def save_data(jobs: list, cursor: sqlite3.Cursor):
     for job in jobs:
-        cursor.execute("INSERT OR IGNORE INTO jobs(id, title, category, company, location, description) VALUES(?,?,?,"
-                       "?,?,?);",
+        cursor.execute("INSERT OR IGNORE INTO jobs(id, title, category, company, location, description) "
+                       "VALUES(?,?,?,?,?,?);",
                        [job['id'], job['title'], job['category'].get('label'), job['company'].get('display_name'),
                         job['location'].get('display_name'), job['description']])
 
@@ -219,16 +156,26 @@ def setup_database(cursor: sqlite3.Cursor):
     cursor.execute(create_statement)
 
 
+def get_params():
+    technology = input("what technology?:")
+    location = input("what location?:")
+    salary_min = input("salary minimum?:")
+    return technology, location, salary_min
+
+
 def main():
     app = QApplication(sys.argv)
+    params = get_params()
+    loc = f"https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=18381bc0&app_key=f20a9d4e1c0d42e8d120af190ecfb44d" \
+          f"&results_per_page=20&what={params[0]}&where={params[1]}&salary_min={params[2]}"
+    print(loc)
     connection = sqlite3.connect("jobs.db")
     cursor = connection.cursor()
     setup_database(cursor)
-    data = get_data()
+    data = get_data(loc)
     save_data(data, cursor)
     connection.commit()
     connection.close()
-    display_data(data)
     write_data(data, "jobs_data.txt")
     window = display_data(data)
     window.show()
